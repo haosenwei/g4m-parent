02bdfad1101b13a481d3cec2c0c4ae75d86b73ad
haosenwei <961970674@qq.com>
2018-08-20 19:02:17 +0800

    22
    22

--- g4m-config-server/src/main/java/com/g4m/SpringConfigServerApplication.java
+++ g4m-config-server/src/main/java/com/g4m/SpringConfigServerApplication.java
@@ -2,11 +2,13 @@ package com.g4m;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableConfigServer
 public class SpringConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringConfigServerApplication.class, args);
--- g4m-config/src/main/java/org/g4m/config/ConfigServerApplication.java
+++ g4m-config/src/main/java/org/g4m/config/ConfigServerApplication.java
@@ -1,18 +1,27 @@
 package org.g4m.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @Configuration
 @EnableAutoConfiguration
 @EnableDiscoveryClient
-@EnableConfigServer
+@RestController
 public class ConfigServerApplication {
 
+	@Value("${version}")
+	private String version;
+	
+	@RequestMapping("/")
+	public String getVersion() {
+		return "version :" + version ;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
