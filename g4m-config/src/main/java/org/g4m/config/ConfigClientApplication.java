a14f5842b7794a54c0905529a35ec24f2b111c55
haosenwei <961970674@qq.com>
2018-08-25 19:06:37 +0800

    111

--- g4m-config/src/main/java/org/g4m/config/ConfigClientApplication.java
+++ g4m-config/src/main/java/org/g4m/config/ConfigClientApplication.java
@@ -3,6 +3,7 @@ package org.g4m.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @EnableEurekaClient
+@RefreshScope
 public class ConfigClientApplication {
 
-	@Value("${version}")
-	private String version;
 	
 	@RequestMapping("/")
 	public String getVersion() {
-		return "version :" +version ;
+		return "version :" ;
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApplication.class, args);
--- g4m-eureka/src/main/java/com/g4m/EurekaApplication.java
+++ g4m-eureka/src/main/java/com/g4m/EurekaApplication.java
@@ -3,18 +3,11 @@ package com.g4m;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 @EnableEurekaServer
 public class EurekaApplication {
 
-	@RequestMapping("/")
-	public String home() {
-		return "Hello World";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
