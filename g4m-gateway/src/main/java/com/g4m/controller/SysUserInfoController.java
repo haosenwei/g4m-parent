f3c6c0f1274e22cffbd42d053b7eeb66d3cbbb4c
961970674@q.com <961970674@qq.com>
2018-08-28 14:04:24 +0800

    12

---
+++ g4m-gateway/src/main/java/com/g4m/controller/SysUserInfoController.java
@@ -0,0 +1,38 @@
+package com.g4m.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import io.swagger.annotations.Api;
+
+@Api(value = "检查模板操作", description = "检查模板操作")
+@Controller
+public class SysUserInfoController {
+
+	@RequestMapping("/test")
+	public String home(String name) {
+		System.out.println("---"+name);
+
+		return "Hello World";
+	}
+
+	@RequestMapping(value="/login",method=RequestMethod.POST)
+	public ModelAndView loginPost(){
+		return new ModelAndView("login");
+	}
+
+	@RequestMapping(value="/login",method=RequestMethod.GET)
+	public ModelAndView loginGet(){
+		return new ModelAndView("login");
+	}
+	@RequestMapping(value="/reg",method=RequestMethod.POST)
+	public ModelAndView regPost(){
+		return new ModelAndView("reg");
+	}
+	@RequestMapping(value="/reg",method=RequestMethod.GET)
+	public ModelAndView regGet(){
+		return new ModelAndView("reg");
+	}
+}
