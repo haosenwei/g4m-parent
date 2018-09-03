ab87c1e3d7b511f73c560275acb57920ef079dd9
haosenwei <961970674@qq.com>
2018-09-03 23:23:16 +0800

    1

---
+++ g4m-admin/src/main/java/com/g4m/conf/HtmlAppender.java
@@ -0,0 +1,57 @@
+package com.g4m.conf;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import org.apache.logging.log4j.core.Filter;
+import org.apache.logging.log4j.core.Layout;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.appender.AbstractAppender;
+import org.apache.logging.log4j.core.appender.FileAppender;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.config.plugins.PluginAttribute;
+import org.apache.logging.log4j.core.config.plugins.PluginElement;
+import org.apache.logging.log4j.core.config.plugins.PluginFactory;
+import org.apache.logging.log4j.core.layout.PatternLayout;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.g4m.websocket.WebSocketTest;
+
+@Plugin(name = "HtmlAppender", category = "Core", elementType = "appender", printObject = true)
+public class HtmlAppender extends AbstractAppender {
+
+
+	@Autowired
+	WebSocketTest webSocketTest;
+	
+	protected HtmlAppender(String name, Filter filter, Layout<? extends Serializable> layout) {
+		super(name, filter, layout);
+	}
+
+	@Override
+	public void append(LogEvent event) {
+		final byte[] bytes = getLayout().toByteArray(event);
+		try {
+			webSocketTest.sendMessage(new String(bytes));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	/*  接收配置文件中的参数 */
+	@PluginFactory
+	public static AbstractAppender createAppender(@PluginAttribute("name") String name,
+			@PluginElement("Filter") final Filter filter,
+			@PluginElement("Layout") Layout<? extends Serializable> layout,
+			@PluginAttribute("ignoreExceptions") boolean ignoreExceptions) {
+		if (name == null) {
+			LOGGER.error("no name defined in conf.");
+			return null;
+		}
+		if (layout == null) {
+			layout = PatternLayout.createDefaultLayout();
+		}
+		return new HtmlAppender(name, filter, layout);
+	}
+}
--- g4m-admin/src/main/java/com/g4m/controller/TestController.java
+++ g4m-admin/src/main/java/com/g4m/controller/TestController.java
@@ -1,14 +1,37 @@
 package com.g4m.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TestController {
+	
+	private final static Logger logger = LoggerFactory.getLogger(TestController.class);
+	
+	@Value("server.port:0")
+	private String serverport;
+	
 	@RequestMapping(value="/a")
 	@ResponseBody
 	public String a() {
-		return "hello";
+		logger.info("aaa");
+		return "hello"+serverport;
 	}
+	
+	@RequestMapping("/log")
+	public ModelAndView log(){
+		logger.info("aaa");
+		return new ModelAndView("log/log");
+	}
+	@RequestMapping("/home")
+	public ModelAndView home(){
+		logger.info("home");
+		return new ModelAndView("home");
+	}
+	
 }
