4883d3ac1fffc7544b0b66c1ae031aa7cea86484
haosenwei <961970674@qq.com>
2019-02-27 14:44:47 +0800

    123

--- g4m-gateway/src/main/java/com/g4m/conf/CustomCookiesSerializer.java
+++ g4m-gateway/src/main/java/com/g4m/conf/CustomCookiesSerializer.java
@@ -1,7 +1,6 @@
 package com.g4m.conf;
 
 import org.springframework.session.web.http.DefaultCookieSerializer;
-import org.springframework.stereotype.Component;
 
 public class CustomCookiesSerializer extends DefaultCookieSerializer{
 
