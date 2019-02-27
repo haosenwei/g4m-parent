e0533103fb74f3d7b0049f97e3b012ff6c3a6250
haosenwei <961970674@qq.com>
2019-02-27 14:46:15 +0800

    1

--- g4m-sleuth/src/main/java/com/g4m/SpringConfigServerApplication.java
+++ g4m-sleuth/src/main/java/com/g4m/SpringConfigServerApplication.java
@@ -4,12 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-import zipkin2.server.internal.EnableZipkinServer;
-
-
 
 @SpringBootApplication
-@EnableZipkinServer
+/*@EnableZipkinServer*/
 @EnableEurekaClient
 public class SpringConfigServerApplication {
     public static void main(String[] args) {
