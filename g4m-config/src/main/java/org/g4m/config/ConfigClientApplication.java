b99e4cd8a256f7c111fa37c59fce229e656c5e2b
haosenwei <961970674@qq.com>
2019-02-28 17:55:47 +0800

    1

--- g4m-config/src/main/java/org/g4m/config/ConfigClientApplication.java
+++ g4m-config/src/main/java/org/g4m/config/ConfigClientApplication.java
@@ -1,10 +1,12 @@
 package org.g4m.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -15,9 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class ConfigClientApplication {
 
 	
+	@Value("${version}")
+	private String version;
+	
 	@RequestMapping("/")
+	@ResponseBody
 	public String getVersion() {
-		return "version :" ;
+		return "version :" + version;
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApplication.class, args);
